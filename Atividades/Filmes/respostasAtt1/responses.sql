e25ea3df85aa5b4c02bc412383491651224267a9
allansmithll <allanammancio2004@gmail.com>
2023-03-21 09:22:46 -0300

    Tarefa 3: concluída

--- Atividades/Filmes/respostasAtt1/responses.sql
+++ Atividades/Filmes/respostasAtt1/responses.sql
@@ -115,7 +115,7 @@ From filme f
 	on f.codcateg = c.codcateg
 Where f.codcateg is null;
 
--- Tarefa 03 - 20/03/2023
+-- Tarefa 03 - 21/03/2023
 
 -- Questão 1
 select f.titulo
@@ -133,3 +133,42 @@ where exists
  	 from estudio e
   	 where f.codest = e.codest and nomeest like 'P%')
 order by f.titulo;
+
+select * from categoria;
+select * from filme;
+
+-- Questão 3
+select c.desccateg
+from Categoria c
+where exists
+	(select f.codcateg
+	 from Filme f
+	 where f.codcateg = c.codcateg);
+
+select * from artista;
+select * from personagem;
+
+-- Questão 4
+select a.nomeart
+from Artista a
+where exists
+	(select p.codart
+	 from Personagem p
+	 where p.codart = a.codart and nomepers = 'Natalie');
+
+-- Questão 5
+select a.nomeart
+from Artista a
+where exists
+	(select p.codart
+	 from Personagem p
+	 where p.codart = a.codart);
+	 
+-- Questão 6
+select a.nomeart
+from Artista a 
+	JOIN Personagem p
+	ON a.codart = p.codart
+where p.cache > 
+			(select avg(cache)
+			 from Personagem);
